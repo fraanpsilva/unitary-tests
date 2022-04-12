45ed2c79c3f07fc86f7ff8703f16e90d2e8ae2dd
Francilene Silva <francilenesilva.fps10@gmail.com>
2022-04-12 15:05:43 +0100

    Create project

---
+++ src/main/java/br/com/fraanps/apifortests/ApiForTestsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fraanps.apifortests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiForTestsApplication.class, args);
+    }
+
+}
