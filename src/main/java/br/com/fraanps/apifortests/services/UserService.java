936a9b87d8105a1f3e528e58f872e16167dab891
Francilene Silva <francilenesilva.fps10@gmail.com>
2022-04-14 23:19:17 +0100

    Endpoint findAll for user

--- src/main/java/br/com/fraanps/apifortests/services/UserService.java
+++ src/main/java/br/com/fraanps/apifortests/services/UserService.java
@@ -2,7 +2,10 @@ package br.com.fraanps.apifortests.services;
 
 import br.com.fraanps.apifortests.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 }
