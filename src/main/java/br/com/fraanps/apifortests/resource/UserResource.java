5ad570cdba0fe41dd1775702c3dab067dbea83e4
Francilene Silva <francilenesilva.fps10@gmail.com>
2022-04-16 22:52:31 +0100

    Exception e-mail already registered in the update

--- src/main/java/br/com/fraanps/apifortests/resource/UserResource.java
+++ src/main/java/br/com/fraanps/apifortests/resource/UserResource.java
@@ -45,4 +45,12 @@ public class UserResource {
         return ResponseEntity.created(uri).build();
 
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO obj){
+        obj.setId(id);
+        User newObj = userService.update(obj);
+        return ResponseEntity.ok().body(mapper.map(newObj, UserDTO.class));
+
+    }
 }
